e79f70c06f51736534dc619072881d3628b9a3b7
Natalia Ramirez Roman <270037658@bun2-206-16.acg.ac.nz>
2021-03-23 15:34:28 +1300

    javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log('hello');
+$(document).ready(function(){
+    $.ajax({
+    url: 'api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}',
+    type: 
+    datatype:
+
+    })
+    
+
+});
