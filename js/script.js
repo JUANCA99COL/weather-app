ceceb100706e98fef5c2b72d49805e4d06094632
Juan Camilo CARO RODRIGUEZ <270024041@bun2-206-07.acg.ac.nz>
2021-03-25 13:45:02 +1300

    search and weather

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-$(document).ready(function(){
-    $.ajax({
-    url: 'api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}',
-    type: 
-    datatype:
+let cityResults = document.querySelector(".results")
+let button = document.querySelector(".searchbtn")
 
-    })
-    
+button.addEventListener("click", () => {
+  
+    showResults = !showResults
+  if (showResults === true) {
+    cityResults.style.display = "inline-block"
+  } else {
+    cityResults.style.display = "none"
+  }
 
-});
+  
+})
+
+let showResults = false
